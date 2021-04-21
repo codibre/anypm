fe0d3758d966ae2bd166d682b511b97be96afdbd
Thiago Santos <tos.oliveira@gmail.com>
2021-04-21 15:28:46 -0300

    creating experimental nvmrc command

--- src/lib/nvmrc.ts
+++ src/lib/nvmrc.ts
@@ -1,27 +1,8 @@
+import { mountNpmCommand } from './mount-npm-command';
 import { replaceNpm } from './replace-npm';
-import { runCmd } from './run-cmd';
+import { runScriptFactory } from './run-script-factory';
 
 export async function nvmrc() {
-	await runCmd(`NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-loadnvmrc() {
-  local node_version="$(nvm version)"
-  local nvmrc_path="$(nvm_find_nvmrc  )"
-
-  if [ -n "$nvmrc_path" ]; then
-    local nvmrc_node_version=$(nvm version "$(cat "\${nvmrc_path}")")
-
-    if [ "$nvmrc_node_version" = "N/A" ]; then
-      nvm install
-    elif [ "$nvmrc_node_version" != "$node_version" ]; then
-      nvm use
-    fi
-  elif [ "$node_version" != "$(nvm version default)" ]; then
-    echo "Reverting to nvm default version"
-    nvm use default
-  fi
-}
-loadnvmrc`);
+	await runScriptFactory(() => [mountNpmCommand('sh', 'apply-nvmrc.sh', [])])();
 	await replaceNpm();
 }
