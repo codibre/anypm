cd5d1ba92881ababdb8ace361b105d31ba6f6441
Thiago Santos <tos.oliveira@gmail.com>
2021-04-21 15:13:27 -0300

    trying ot make nvmrc work

--- src/lib/nvmrc.ts
+++ src/lib/nvmrc.ts
@@ -1,7 +1,10 @@
 import { runCmd } from './run-cmd';
 
 export async function nvmrc() {
-	await runCmd(`loadnvmrc() {
+	await runCmd(`NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+loadnvmrc() {
   local node_version="$(nvm version)"
   local nvmrc_path="$(nvm_find_nvmrc  )"
 
