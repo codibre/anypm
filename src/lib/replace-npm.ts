ffcb9f999c679f12104891254922fecf9842f6fa
Thiago Santos <tos.oliveira@gmail.com>
2021-06-11 10:20:40 -0300

    fixing replace npm

--- src/lib/replace-npm.ts
+++ src/lib/replace-npm.ts
@@ -1,7 +1,34 @@
-import { readFileSync } from 'fs';
-
 export async function replaceNpm() {
 	console.info('If you use Linux, add this to your .zshrc or .bashrc:');
 	console.info('-----------------------------------------------------');
-	console.info(readFileSync('apply-nvmrc.sh').toString());
+	console.info(`replacenpm() {
+  alias realnpm=$(which npm)
+  alias npm=anypm
+}
+
+loadnvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc  )"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "\${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+      replacenpm()
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+      replacenpm()
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+    replacenpm()
+  fi
+}
+loadnvmrc
+cd() {
+  builtin cd "$1"
+  loadnvmrc
+}`);
 }
